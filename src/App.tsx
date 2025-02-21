316fbafab33d283ceb1a87fbae1d667acf185f8b
Md Shifatul Ahsan Apurba <apurbaahsan@gmail.com>
2025-02-21 13:50:57 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -431,7 +431,7 @@ function App() {
                     </div>
                   ) : (
                     <div className="h-full relative rounded-xl overflow-hidden">
-                      <div className={`${isMobile ? 'w-full aspect-square object-cover' : ''}`}>
+                      <div className={`${isMobile ? 'h-full aspect-square object-cover' : ''} h-full`}>
                         <Webcam
                           ref={webcamRef}
                           audio={false}
